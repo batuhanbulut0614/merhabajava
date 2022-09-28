fb800d52613b255ed9b1cbd82004b8c349f790ac
batuhanbulut0614 <113903075+batuhanbulut0614@users.noreply.github.com>
2022-09-28 12:04:38 +0300

    Initial commit

---
+++ src/merhabajava/Main.java
@@ -0,0 +1,12 @@
+package merhabajava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Batuhan Bulut ");
+		System.out.println("Merhaba Java");
+		System.out.println("2023");
+
+	}
+
+}
